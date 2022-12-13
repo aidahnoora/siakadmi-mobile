30f7d46f704078a2d498a7bf2eca891afbb6d7b4
A'idah Noor <webinaraidah30@gmail.com>
2022-12-13 14:51:09 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:siakad/pages/sign_in.dart';
+import 'package:siakad/pages/splash_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => SplashPage(),
+        '/sign-in': (context) => SignInPage(),
+      },
+    );
+  }
+}
